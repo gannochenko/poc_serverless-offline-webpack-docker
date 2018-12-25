ee38cfd45b4201517271c24a161ed7caeac87b27
Sergei <gannochenko.sv@gmail.com>
2018-12-25 16:45:05 +0100

    files moved

--- lambda-app/webpack.config.js
+++ lambda-app/webpack.config.js
@@ -5,25 +5,23 @@ const slsw = require('serverless-webpack');
 module.exports = {
   entry: slsw.lib.entries,
   target: 'node',
+  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
   externals: [nodeExternals()],
   output: {
     libraryTarget: 'commonjs',
-    path: path.join(__dirname, 'dst'),
+    // pay attention to this
+    path: path.join(__dirname, '.webpack'),
     filename: '[name].js',
   },
   module: {
     rules: [
-      {
-        test: /\.(graphql|gql)$/,
-        exclude: /node_modules/,
-        loader: 'graphql-tag/loader',
-      },
       {
         test: /\.js$/,
         use: [
           {
             loader: 'babel-loader',
             options: {
+              // ... and this
               presets: [['@babel/env', { targets: { node: '8.10' } }]],
             },
           },
